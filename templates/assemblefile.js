86252309f324e86ee65fecd9b58a1bfe8b6bc0a4
Rhyspacker <rhys.pacs@yahoo.com>
2018-10-16 21:01:33 +0100

    handlebars OR and other conditionals added

--- templates/assemblefile.js
+++ templates/assemblefile.js
@@ -65,6 +65,33 @@ app.task('html', function() {
 	  handlebars: handlebars
 	});
 
+  handlebars.registerHelper({
+    eq: function (v1, v2) {
+        return v1 === v2;
+    },
+    ne: function (v1, v2) {
+        return v1 !== v2;
+    },
+    lt: function (v1, v2) {
+        return v1 < v2;
+    },
+    gt: function (v1, v2) {
+        return v1 > v2;
+    },
+    lte: function (v1, v2) {
+        return v1 <= v2;
+    },
+    gte: function (v1, v2) {
+        return v1 >= v2;
+    },
+    and: function () {
+        return Array.prototype.slice.call(arguments).every(Boolean);
+    },
+    or: function () {
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    }
+  });
+
 	app.partials(['src/_components/**/*.hbs']);
   app.layouts('src/_layouts/*.hbs');
   app.pages('src/pages/*.hbs');
